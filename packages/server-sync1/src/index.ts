9766f938c8983ed2763bb8af88b80f9863b339f4
deniz <43997085+deniz-blue@users.noreply.github.com>
2026-06-04 10:57:25 +0300

    sync1: add logging..

--- packages/server-sync1/src/index.ts
+++ packages/server-sync1/src/index.ts
@@ -1,33 +1,14 @@
 import { Hono } from "hono";
 import { basicAuth } from "hono/basic-auth";
-import { drizzle } from "drizzle-orm/better-sqlite3";
-import Database from "better-sqlite3";
 import { zValidator } from "@hono/zod-validator";
-import { sqliteTable,  } from "drizzle-orm/sqlite-core";
-import { dz } from "@vantage/db";
 import { z } from "zod";
 import { eq } from "drizzle-orm";
-
-const sync1 = sqliteTable("sync1", {
-	id: dz.uuid("id").primaryKey(),
-	data: dz.jsonb("data").notNull(),
-	updatedAt: dz.timestamp("updated_at").notNull(),
-	deleted: dz.bool("deleted").notNull().default(false),
-});
-
-const sqlite = new Database("sync.db");
-const db = drizzle(sqlite, { schema: { sync1 } });
-
-sqlite.exec(`
-CREATE TABLE IF NOT EXISTS sync1 (
-	id TEXT PRIMARY KEY,
-	data BLOB NOT NULL,
-	updated_at INTEGER NOT NULL,
-	deleted INTEGER NOT NULL DEFAULT 0
-);
-`);
+import { logger } from "hono/logger";
+import { db, sync1 } from "./db";
 
 const app = new Hono<{ Variables: { SECRET_KEY: string } }>()
+
+app.use(logger());
 
 app.get("/", (c) => {
 	return c.json({ message: "Sync1 Server running" });
