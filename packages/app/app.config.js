ff705d036ae88760d94d1fa6d622769810dea00b
deniz <43997085+deniz-blue@users.noreply.github.com>
2026-08-02 13:48:37 +0300

    v5

--- packages/app/app.config.js
+++ packages/app/app.config.js
@@ -2,8 +2,8 @@ const DEV = process.env.APP_VARIANT === "development";
 
 if (DEV) console.log("[app.config.js] Running in development mode");
 
-const version = "0.1.0";
-const versionCode = 4;
+const version = "0.1.1";
+const versionCode = 5;
 
 export default {
 	/** @type {import("expo/config").ExpoConfig} */
